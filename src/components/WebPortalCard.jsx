872ba73d1d5b7260350299149af77f97ba352c87
Gefferson Casasola <gcasasolah@uni.pe>
2023-05-04 21:23:06 -0500

    Add web portal card

--- src/components/WebPortalCard.jsx
+++ src/components/WebPortalCard.jsx
@@ -1,32 +1,29 @@
-import React from "react";
+import React from 'react'
 import {AiFillEdit} from 'react-icons/ai'
 import {MdDelete} from 'react-icons/md'
 
-const JobCard = ({ job }) => {
+const WebPortalCard = ({ webPortal }) => {
   return (
     <div className="bg-white rounded-2xl p-6 flex gap-4 drop-shadow-lg mt-2">
       {/* Icon */}
       <div className="w-[10%] flex items-center justify-center">
-        <img src={job.portal_logo} alt="portal-logo" className="rounded-md"/>
+        <img src={webPortal.logo} alt="portal-logo" className="rounded-md"/>
       </div>
       {/* Tema */}
-      <div className="w-[60%]">
+      <div className="w-[80%]">
         <h3 className="text-lg flex items-center gap-4 mb-2">
-          {job.name}{" "}<span className="text-xs px-1 py-2 bg-purple-100 text-purple-600 font-bold rounded-md">Remote</span>
-          <span className="text-xs px-1 py-2 bg-green-100 text-green-600 font-bold rounded-md"><a href={job.url}>Link</a></span>
+          {webPortal.name}{" "}
+          <span className="text-xs px-1 py-2 bg-green-100 text-green-600 font-bold rounded-md"><a href={webPortal.url}>Link</a></span>
         </h3>
-        <p className="text-gray-500 text-xs">{job.company}</p>
+        <p className="text-gray-500 text-xs">{webPortal.prefixe}</p>
       </div>
-      <div className="w-[20%]">
-        <h4 className="text-blue-800 mb-2 text-xs">{job.webPortal}</h4>
-        <p className="text-blue-400 text-xs">{job.publicationDate}</p>
-      </div>
+      
       <div className="w-[10%] flex flex-col">
         <button className="bg-teal-500 hover:bg-teal-500 text-white font-bold py-1 px-2 rounded-md mb-2 text-xs flex flex-row items-center"><AiFillEdit className="mr-2"/>Edit</button>
         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-md text-xs flex flex-row items-center"><MdDelete className="mr-2"/>Delete</button>
       </div>
     </div>
   );
-};
+}
 
-export default JobCard;
+export default WebPortalCard
--- src/components/WebPortals.jsx
+++ src/components/WebPortals.jsx
@@ -1,11 +1,61 @@
 import React from "react";
 import Layout from "./Layout";
+import { useState, useEffect } from "react";
 import ListHeader from "./ListHeader";
+import refreshToken from "../services/refreshtoken";
+import WebPortalCard from "./WebPortalCard";
+import axios from "axios";
+
+
 const WebPortals = () => {
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  const authTokens = JSON.parse(localStorage.getItem("authTokens"));
+  const [accessToken, setAccessToken] = useState(authTokens.access);
+  const [webPortals, setWebPortals] = useState([]);
+  const [cachedData, setCachedData] = useState({});
+  const BASE_URL = "http://127.0.0.1:8000/web-portal/";
+
+  useEffect(() => {
+    const today = new Date().toISOString().replace("T", " ").slice(0, 19);
+
+    const fetchWebPortals = async () => {
+      if (userData.expirated_date === today) {
+        const newAccessToken = await refreshToken(authTokens.refresh);
+        setAccessToken(newAccessToken);
+      }
+
+      if (cachedData.webPortals) {
+        setWebPortals(cachedData.webPortals);
+      } else {
+        try {
+          const response = await axios.get(BASE_URL, {
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: "Bearer " + accessToken,
+            },
+          });
+          const data = response.data;
+          setCachedData({ webPortals: data.results });
+          setWebPortals(data.results);
+        } catch (error) {
+          console.error(err);
+        }
+      }
+    };
+
+    fetchWebPortals();
+  }, [authTokens, cachedData]);
+  
   return (
     <Layout>
       <div className="app">
         <ListHeader listName={" 💻Web Portals list"} />
+        <div className="mt-4">
+          {webPortals.map((webPortal) => (
+            <WebPortalCard key={webPortal.id} webPortal={webPortal} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
